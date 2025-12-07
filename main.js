c7fe49aee5e3c148117ecb5608f53c58ab9b71af
amirholya01 <amirholya01@gmail.com>
2025-12-08 00:40:49 +0330

    swagger.config

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const swaggerConfig = require('./src/config/swagger.config');
+
 function main(){
     const app = express();
     const PORT = process.env.PORT;
+    swaggerConfig(app);
     require('./src/config/mongoose.config');
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
---
+++ src/config/swagger.config.js
@@ -0,0 +1,20 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+function swaggerConfig(app){
+    const swaggerDocument = swaggerJsDoc({
+        swaggerDefinition: {
+            openapi: "3.0.1",
+            info: {
+                title: "Bazaar Web Application",
+                version: "1.0.0",
+                description: "Node.js, Express.js, MongoDB"
+            },
+        },
+        apis: []
+    });
+    const swagger = swaggerUi.setup(swaggerDocument, {});
+    app.use("/swagger", swaggerUi.serve, swagger);
+}
+
+module.exports = swaggerConfig;
