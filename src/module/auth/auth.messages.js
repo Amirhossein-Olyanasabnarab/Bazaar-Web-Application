ead41aa22e210d9f5fb86efc4b5314e098b992ca
amirholya01 <amirholya01@gmail.com>
2025-12-10 08:48:25 +0330

    sendOtp service

--- src/module/auth/auth.messages.js
+++ src/module/auth/auth.messages.js
@@ -1,5 +1,6 @@
 authMessages = {
     "sendOtp": "Sending OTP was successfully",
-    "notFound:": "The user was not found"
+    "notFound:": "The user was not found",
+    "otpNotExpired": "The OTP has not expired yet"
 }
 module.exports = authMessages;
--- src/module/auth/auth.service.js
+++ src/module/auth/auth.service.js
@@ -1,8 +1,8 @@
 const autoBind = require('auto-bind');
 const UserModel = require('../user/user.model');
-const createError = require('http-errors');
+const createHttpError = require('http-errors');
 const authMessages = require('./auth.messages');
-
+const {randomInt} = require('crypto');
 class AuthService{
     #model;
     constructor(){
@@ -10,9 +10,29 @@ class AuthService{
         this.#model = UserModel;
     }
 
+    async sendOtp(mobile){
+        const user = await this.checkExistByMobile(mobile);
+        const now = new Date().now();
+
+        const otp = {
+            code: randomInt(10000,99999),
+            expireIn: now + (1000 * 60 * 2) // 2 minutes
+        }
+
+        if(!user){
+           return await this.#model.create({mobile,otp});
+        }
+    
+        if(user.otp.expireIn > now) throw new createHttpError.BadRequest(authMessages.otpNotExpired);
+        
+        user.otp = otp;
+        await user.save();
+        return user;
+    }
+
     async checkExistByMobile(mobile){
         const user = await this.#model.findOne({mobile});
-        if(!user) throw new createError.NotFound(authMessages.notFound);
+        if(!user) throw new createHttpError.NotFound(authMessages.notFound);
         return user;
     }
 }
